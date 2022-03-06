da3c10fed384c67589e27b05c3264181e5217b44
NessaAz <vanessa.azenwa@gmail.com>
2022-03-07 02:34:37 +0300

    I further modified the button alerts for birthday output

--- script.js
+++ script.js
@@ -2,3 +2,19 @@ $(document).ready()
 {
     $("#myBirthDay").mask("99/99/9999")
 };
+
+function getAkanName(){
+    let days = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
+    let maleAkanNames = ["Kwasi","Kwadwo","Kwabena","Kwaku","Yaw","Kofi","Kwame"];
+    let femaleAkanNames = ["Akosua","Adwoa","Abenaa","Akua","Yaa","Afua","Ama"];
+    let myBirthday = document.getElementById("myBirthDate").value;
+    let myGender = document.getElementsByName("gender");
+    let dateOfBirth = new Date(myBirthday);
+    let dayOfTheWeek = dateOfBirth.getDay();
+
+    if(myBirthday === "")
+    {
+        document.getElementById('message').innerHTML = "<div class=\"alert alert-danger\"><button type=\"button\" class=\"close\" data-dismiss=\"alert\" aria-hidden=\"true\">x</button><strong>Oh snap!</strong> You Didn't Submit a Valid Date!</div>";
+        $('#message').addClass("animated shake");
+    }
+
